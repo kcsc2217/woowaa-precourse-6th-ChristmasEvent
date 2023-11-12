b8aa095274888f9e895bd56e23322bc59271d805
kcsc2217 <k12002@nate.com>
2023-11-12 23:46:37 +0900

    메뉴 상수 ENUM 으로 구현

---
+++ src/main/java/Model/Menu.java
@@ -0,0 +1,43 @@
+package Model;
+
+public enum Menu {
+	
+	//애피타이저
+	YENGSONGSOUP("양송이 수프", 6000),
+	TAPAS("타파스", 5500),
+	 CAESARSALAD("시저샐러드", 8000),
+	 
+	 // 메인
+	    T_BONE_STEAK("티본스테이크", 55000),
+	    BBQ_RIB("바비큐립", 54000),
+	    SEAFOOD_PASTA("해산물파스타", 35000),
+	    CHRISTMAS_PASTA("크리스마스파스타", 25000),
+
+	    // 디저트
+	    CHOCO_CAKE("초코케이크", 15000),
+	    ICE_CREAM("아이스크림", 5000),
+
+	    // 음료
+	    ZERO_COLA("제로콜라", 3000),
+	    RED_WINE("레드와인", 60000),
+	    CHAMPAGNE("샴페인", 25000);
+	
+	private final String name;
+	private final int price;
+	
+	Menu(String name, int price){
+		this.name = name;
+		this.price = price;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	 
+	
+
+}
